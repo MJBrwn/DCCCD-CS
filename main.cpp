bb202fcae66fe772cb9d344d2688fc467a55138b
MJbrwn <brownjosiah09@gmail.com>
2017-10-15 20:37:21 -0500

    updated main

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iomanip>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+const double PI = 3.14159;
+
+int main()
+{
+
+    //Initialize variables
+	double height, radius;
+
+	//Find radius
+    cout << "Enter the radius of the base of the cylinder (cm): ";
+        cin >> radius;
+        cout << endl;
+
+    //Find height
+	cout << "Enter the height of the cylinder (cm): ";
+        cin >> height;
+        cout << endl;
+
+
+	//Volume of Cylinder
+	cout << "Volume of the cylinder in centimeters = "
+
+		<< fixed << showpoint << setprecision(2)
+		<< PI * pow(radius, 2.0)* height << endl;
+
+		
+
+    //Surface area of Cylinder
+		cout << "Surface area: "
+
+		<< 2 * PI * radius * height + 2 * PI * pow(radius, 2.0)
+		<< endl;
+	cout << fixed << showpoint << setprecision(2);
+}
+
+
+
